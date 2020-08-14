0f2d6561bda5e7b134d60e4987b7c1852f160a37
makers <bbiehn1@gmail.com>
2020-08-13 18:00:07 -0600

    afternoon full stack practice

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import './reset.css';
+import { Switch, Router, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import FrontPage from './components/FrontPage';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route path='/front_page' component={FrontPage} />
+          <Route path='/profile' component={Profile} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
